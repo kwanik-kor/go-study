8a5817cba39921ee06ecaeb542852da812aa761a
kwanik-kor <rhksdlr134@naver.com>
2022-12-28 21:35:16 +0900

    feat: GoLang에서의 Export 처리
    - 함수명을 대문자로 작성할 경우 Export 할 수 있다

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"go-study/something"
 	"rsc.io/quote"
 )
 
@@ -10,5 +11,9 @@ import (
  *  - Java의 main(String[] args) 함수와 동일한 기능을 수행한다.
  */
 func main() {
-	fmt.Println(quote.Go())
+	fmt.Println(quote.Hello())
+
+	// - Function 을 대문자로 작성하는 경우 export 할 수 있는 함수가 된다.
+	something.SayHello()
+	// something.sayBye()
 }
