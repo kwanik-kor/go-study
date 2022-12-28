4e2573d5d23016a8a32686790a60e6d3d14ae6a9
kwanik-kor <rhksdlr134@naver.com>
2022-12-28 21:29:49 +0900

    feat: main.go가 필요한 이유

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
-package go_study
+package main
 
 import (
 	"fmt"
 	"rsc.io/quote"
 )
 
+/*
+ * 프로젝트의 Compile이 필요한 경우 main.go 만들기.
+ *  - Java의 main(String[] args) 함수와 동일한 기능을 수행한다.
+ */
 func main() {
 	fmt.Println(quote.Go())
 }
